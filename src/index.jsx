c3c4eb67003d1697f1448950d28b17fa1e10bd22
Azamat <azamat.home@gmail.com>
2022-08-01 23:19:56 +0600

    changed render to React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import './index.scss'
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from "react-dom/client";
 import App from './App'
 import { Provider } from 'react-redux'
-import {store} from './redux/store'
+import { store } from './redux/store'
 
-render(
-   <Provider store={store}>
-      <App />
-   </Provider>
-   , document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+   <React.StrictMode>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </React.StrictMode>
+);
+
